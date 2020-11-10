416029e4d680857b9bbba11c43084414208ff6bf
yehoudazoulay <yehoudazoulay@gmail.com>
2020-11-10 17:39:58 +0100

    Setup and Job1

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,5 +1,6 @@
 package com.opstty;
 
+import com.opstty.job.District;
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
 
@@ -11,7 +12,8 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
-
+            programDriver.addClass("district", District.class,
+                    "A map/reduce program that display all districts with remarkable trees");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
---
+++ src/main/java/com/opstty/reducer/DistrictReducer.java
@@ -0,0 +1,17 @@
+package com.opstty.reducer;
+
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class DistrictReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+        context.write(key, result);
+    }
+}
